6f90cc723bf986f9115f438b1a696de5b19fb9c5
sujita <sujitaregmi189@gmail.com>
2026-01-27 14:17:46 +0545

    Add OTP functionality

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User     
 
-# Create your models here.
+
+class EmailOTP(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    otp = models.CharField(max_length=6)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"OTP for {self.user.email}"
--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login
+from django.contrib.auth import authenticate,login
 from django.contrib import messages
 from .forms import RegisterForm
+import random,uuid
+from django.core.mail import send_mail
+from django.conf import settings
+from .models import LoginOTP
+# from .utils import generate_otp
+from django.contrib.auth.models import User
 
 def register_view(request):
     if request.method == "POST":
         form = RegisterForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
-            user.username = user.email   # email as username
+            user.username = user.email   
             user.save()
 
             login(request, user)
@@ -20,4 +26,70 @@ def register_view(request):
         form = RegisterForm()
 
     return render(request, "accounts/register.html")
+
+def generate_otp():
+    return str(random.randint(100000, 999999))
+
+def login_view(request):
+    if request.method == "POST":
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+
+        try:
+            user = User.objects.get(username=email)
+        except User.DoesNotExist:
+            messages.error(request, "Invalid credentials")
+            return redirect("login")
+
+        user = authenticate(request, username=email, password=password)
+
+        if not user:
+            messages.error(request, "Invalid credentials")
+            return redirect("login")
+
+        otp = generate_otp()
+        LoginOTP.objects.create(user=user, code=otp)
+
+        send_mail(
+            subject="MoodMate Login OTP",
+            message=f"Your OTP is {otp}. It expires in 5 minutes.",
+            from_email="no-reply@moodmate.local",
+            recipient_list=[user.email],
+        )
+
+        request.session["otp_user_id"] = user.id
+        messages.success(request, "OTP sent to your email")
+        return redirect("verify_otp")
+
+    return render(request, "accounts/login.html")
+
+def verify_otp(request):
+    user_id = request.session.get("otp_user_id")
+
+    if not user_id:
+        return redirect("login")
+
+    if request.method == "POST":
+        otp_input = request.POST.get("otp")
+
+        otp_obj = LoginOTP.objects.filter(
+            user_id=user_id,
+            code=otp_input,
+            is_used=False
+        ).last()
+
+        if not otp_obj or otp_obj.is_expired():
+            messages.error(request, "Invalid or expired OTP")
+            return redirect("verify_otp")
+
+        otp_obj.is_used = True
+        otp_obj.save()
+
+        login(request, otp_obj.user)
+        del request.session["otp_user_id"]
+
+        return redirect("dashboard")
+
+    return render(request, "accounts/verify_otp.html")
+
 
